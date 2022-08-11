2c26e93c53c11a972f5ecc28a0d4f8de698d3de4
christina <christinamharris83@gmail.com>
2022-08-11 16:10:35 -0700

    set up typescript, get imports working

---
+++ webfront/src/components/navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Logo from "../../assets/images/logo.tif"
+import {Navigation} from "./style"
+
+const Navbar:React.FC = () => {
+    return (
+        <>
+          <Logo/>
+        </>
+    )
+}
+
+export default Navbar
---
+++ webfront/src/components/navbar/style.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { NavBar } from 'precise-ui';
+import Navbar from ".";
+
+
+export const Navigation = styled(Navbar) `
+
+`;
